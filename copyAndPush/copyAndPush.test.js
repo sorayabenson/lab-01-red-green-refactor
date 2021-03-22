153417c186aedec2d7adc58dd8da92d10d0be0a7
Soraya Benson <soraya.benson@gmail.com>
2021-03-22 15:25:28 -0700

    copyAndPush ueses spread to copy and append, test confirms original array stays the same

--- copyAndPush/copyAndPush.test.js
+++ copyAndPush/copyAndPush.test.js
@@ -7,5 +7,14 @@ describe('copyAndPush function', () => {
         const newNumbers = copyAndPush(numbers, 4);
 
         expect(newNumbers).toEqual([1, 2, 3, 4]);
+        expect(numbers).toEqual([1, 2, 3]);
+    });
+
+    it('takes in an array, makes a copy of it, and pushes a new item at the end of the array', () => {
+        const books = ['Howl`s Moving Castle', 'Castle in the Air'];
+
+        const newBooks = copyAndPush(books, 'House of Many Ways');
+
+        expect(newBooks).toEqual(['Howl`s Moving Castle', 'Castle in the Air', 'House of Many Ways']);
     });
 });
